150ccf47357dfb844784be936eb3fe1e79fb3a5c
aartibaghel456 <aartibaghel456@gmail.com>
2024-06-27 00:45:34 +0530

    added code for coming soon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ const Home =() =>{
     return(
         <>
         <div className="banner">
-            <img src="/khollab/assets/image/comingsoon2.jpg" alt="checkinout" className="img-responsive"/>
+            <img src="/docs/assets/image/comingsoon2.jpg" alt="checkinout" className="img-responsive"/>
         </div>
         </>
     )
